9b94a3f480ad5d1ccb8695d384be238e9a4db4f2
dilani.alwis <dilani.alwis@gmail.com>
2017-12-26 12:51:36 +0530

    Add db context

---
+++ AdminService/Models/AdminDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminService.Models
+{
+    public class AdminDBContext : DbContext
+    {
+        public AdminDBContext(DbContextOptions<AdminDBContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<SystemUser> SystemUsers { get; set; }
+    }
+}
