f3f646b1bf9e442331201c3338f6a847a4872ed8
dilani.alwis <dilani.alwis@gmail.com>
2018-01-05 16:28:19 +0530

    Add DB migrations

---
+++ AdminService/Migrations/20180105101143_InitialAzureDbMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AdminService.Migrations
+{
+    public partial class InitialAzureDbMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
