c0a9133e819c47d5c525f291c423c12345900975
ThanoshanMV <mvthanoshan9@gmail.com>
2020-02-09 18:52:13 +0530

    Removing unnecessary hyperlink comments

--- UI/build/webpack.prod.conf.js
+++ UI/build/webpack.prod.conf.js
@@ -30,7 +30,6 @@ const webpackConfig = merge(baseWebpackConfig, {
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
-    // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
     }),
